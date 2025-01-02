03b80453b3642e739c720c659dcb770e04e4259d
ThomasLuton <luton.thomas60@gmail.com>
2025-01-02 15:53:54 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SleepCounterComponent } from './sleep-counter/sleep-counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SleepCounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/sleep-counter/sleep-counter.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-sleep-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './sleep-counter.component.html',
+  styleUrl: './sleep-counter.component.css'
+})
+export class SleepCounterComponent implements OnInit {
+
+  isCountDown = signal(true);
+  readonly startDate = new Date('June 19, 2025 14:00:00');
+  now = signal(Date.now());
+  remainingTime = computed(() => (this.startDate.getTime() - this.now()))
+  remainingDays = computed(() => Math.floor(this.remainingTime() / 86400000))
+  remainingHoures = computed(() => Math.floor(this.remainingTime() / 3600000))
+  remainingMinutes = computed(() => Math.floor((this.remainingTime() - this.remainingHoures() * 3600000) / 60000))
+  remainingSeconds = computed(() => Math.floor((this.remainingTime() - (this.remainingHoures() * 3600000 + this.remainingMinutes() * 60000)) / 1000))
+
+  ngOnInit() {
+    setInterval(() => {
+      this.now.update(() => Date.now())
+    }, 1000)
+  }
+
+  switchButton() {
+    if (this.isCountDown()) {
+      this.isCountDown.set(false);
+    } else {
+      this.isCountDown.set(true);
+    }
+  }
+}
