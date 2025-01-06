97ec92d7d3e189c2c633e12280f42a8f15dc9966
ThomasLuton <luton.thomas60@gmail.com>
2025-01-06 16:15:21 +0100

    continue calendar feature

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -9,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class CalendarComponent {
 
+  date = new Date(Date.now())
+  selectMonthId = signal(this.date.getUTCMonth());
+  selectedMonth = computed(() => this.convertMonth(this.selectMonthId()));
+
+  convertMonth(monthId: number): string {
+    switch (monthId) {
+      case 0:
+        return "Janvier";
+      case 1:
+        return "Février";
+      case 2:
+        return "Mars";
+      case 3:
+        return "Avril";
+      case 4:
+        return "Mai";
+      case 5:
+        return "Juin";
+      default:
+        return "";
+    }
+  }
+
+  isFirst(): boolean {
+    if (this.selectMonthId() <= 0) {
+      return true;
+    }
+    return false;
+  }
+
+  isLast(): boolean {
+    if (this.selectMonthId() >= 5) {
+      return true;
+    }
+    return false;
+  }
+
+  onPrevious(): void {
+    if (!this.isFirst()) {
+      this.selectMonthId.update(() => this.selectMonthId() - 1)
+    }
+  }
+
+  onNext(): void {
+    if (!this.isLast()) {
+      this.selectMonthId.update(() => this.selectMonthId() + 1)
+    }
+  }
 }
